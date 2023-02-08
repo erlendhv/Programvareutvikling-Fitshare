90df038ef33369206aaba01b0ecd4bb9db2b91cc
Bendik Norli <bendik.norli@ntnu.no>
2023-02-08 14:04:26 +0100

    create basic css layout

--- fitshare/src/App.tsx
+++ fitshare/src/App.tsx
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          FitShare
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Left-side-bar">
+
+        <div className="Home-icon-box">FS</div>
+        <div className="Groups">
+          Groups
+
+          <div className="Group">
+          </div>
+          <div className="Group">
+          </div>
+          <div className="Group">
+          </div>
+          <div className="Group">
+          </div>
+
+        </div>
+      </div>
+      <div className="Middle">
+        <div className="Top-bar">
+        </div>
+
+      </div>
+      <div className="Right-side-bar">
+        <div className="Programs-button">
+          Programs
+        </div>
+
+        <div className="Friends">
+          Friends
+
+          <div className="Friend">
+            <div className="Friend-profile-pic">FS</div>
+            <div className="Friend-name">Friend 1</div>
+          </div>
+          <div className="Friend">
+            <div className="Friend-profile-pic">FS</div>
+            <div className="Friend-name">Friend 1</div>
+          </div>
+          <div className="Friend">
+            <div className="Friend-profile-pic">FS</div>
+            <div className="Friend-name">Friend 1</div>
+          </div>
+          <div className="Friend">
+            <div className="Friend-profile-pic">FS</div>
+            <div className="Friend-name">Friend 1</div>
+          </div>
+          <div className="Friend">
+            <div className="Friend-profile-pic">FS</div>
+            <div className="Friend-name">Friend 1</div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
