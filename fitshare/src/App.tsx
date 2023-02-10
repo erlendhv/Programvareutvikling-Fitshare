3674746bb9bf9441709e2369393979becc29549a
Erlend Veire <erlendhv@stud.ntnu.no>
2023-02-10 11:57:37 +0100

    Enable liking posts
    Co-authored-by: bendiknorli <@bendinor>

--- fitshare/src/App.tsx
+++ fitshare/src/App.tsx
@@ -5,41 +5,57 @@ import './App.css';
 import ExercisePhoto from './ExercisePhoto.jpeg';
 import FitShareLogo from './FitShareLogo.png';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [currentGroup, setCurrentGroup] = useState("Group 1");
 
   const [groups, setGroups] = useState([
-    { name: "Group 1" },
-    { name: "Group 2" },
-    { name: "Group 3" },
-    { name: "Group 4" },
-    { name: "Group 5" },
-    { name: "Group 6" },
-    { name: "Group 7" },
-    { name: "Group 8" },
+    { id: 0, name: "Group 1" },
+    { id: 1, name: "Group 2" },
+    { id: 2, name: "Group 3" },
+    { id: 3, name: "Group 4" },
+    { id: 4, name: "Group 5" },
+    { id: 5, name: "Group 6" },
+    { id: 6, name: "Group 7" },
+    { id: 7, name: "Group 8" },
   ]);
 
   const [friends, setFriends] = useState([
-    { name: "Friends 1" },
-    { name: "Friends 2" },
-    { name: "Friends 3" },
-    { name: "Friends 4" },
-    { name: "Friends 5" },
-    { name: "Friends 6" },
-    { name: "Friends 7" },
-    { name: "Friends 8" },
-    { name: "Friends 9" },
-    { name: "Friends 10" },
-    { name: "Friends 11" },
-    { name: "Friends 12" },
+    { id: 0, name: "Friends 1" },
+    { id: 1, name: "Friends 2" },
+    { id: 2, name: "Friends 3" },
+    { id: 3, name: "Friends 4" },
+    { id: 4, name: "Friends 5" },
+    { id: 5, name: "Friends 6" },
+    { id: 6, name: "Friends 7" },
+    { id: 7, name: "Friends 8" },
+    { id: 8, name: "Friends 9" },
+    { id: 9, name: "Friends 10" },
+    { id: 10, name: "Friends 11" },
+    { id: 11, name: "Friends 12" },
   ]);
 
   const [posts, setPosts] = useState([
-    { name: "Gunnhild Pedersen", program: [{ workoutName: "Leg day", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }, { workoutName: "Workout 2", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }], image: ExercisePhoto },
-    { name: "Gunnhild Pedersen", program: [{ workoutName: "Pull", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }, { workoutName: "Workout 2", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }], image: "" },
+    { id: uuidv4(), name: "Gunnhild Pedersen", program: [{ workoutName: "Leg day", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }, { workoutName: "Workout 2", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }], image: ExercisePhoto, likes: 0, liked: false, comments: ["Thats crazy!", "No way!", "Dude, that is the most crazy thing I have ever seen in my entire life!"] },
+    { id: uuidv4(), name: "Gunnhild Pedersen", program: [{ workoutName: "Pull", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }, { workoutName: "Workout 2", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }], image: "", likes: 499, liked: true, comments: [] },
   ]);
+
+  function toggleLiked(id: string) {
+    const newPosts = [...posts]
+    const post = newPosts.find(post => post.id === id)
+
+    if (post != null) {
+      post.liked = !post.liked
+      if (post.liked) {
+        post.likes += 1
+      } else {
+        post.likes -= 1
+      }
+    }
+    setPosts(newPosts)
+  }
 
 
   return (
@@ -54,7 +70,7 @@ function App() {
           <strong>Groups</strong>
 
           {groups.map((group) => (
-            <Group name={group.name} />
+            <Group key={group.id} name={group.name} />
           ))}
 
         </div>
@@ -79,7 +95,8 @@ function App() {
         <div className="Group-icon-feed">
 
           {posts.map((post) => (
-            <Post name={post.name} program={post.program} image={post.image} />
+            <Post key={post.id} id={post.id} name={post.name} program={post.program} image={post.image}
+              likes={post.likes} liked={post.liked} comments={post.comments} toggleLiked={toggleLiked} />
           ))}
 
         </div>
@@ -95,7 +112,7 @@ function App() {
           <strong>Friends</strong>
 
           {friends.map((friend) => (
-            <Friend name={friend.name} />
+            <Friend key={friend.id} name={friend.name} />
           ))}
 
         </div>
--- fitshare/src/components/Post.tsx
+++ fitshare/src/components/Post.tsx
@@ -1,23 +1,37 @@
 import React from "react";
 import { FiThumbsUp } from "react-icons/fi";
+import { AiOutlineComment } from "react-icons/ai";
 
-export function Post(props: { name: string, program: { workoutName: string; exercises: { name: string; sets: number; reps: number; }[]; }[], image: string }) {
+export function Post(props: {
+  id: string, name: string,
+  program: { workoutName: string; exercises: { name: string; sets: number; reps: number; }[]; }[],
+  image: string, likes: number, liked: boolean, comments: string[], toggleLiked: (id: string) => void
+}) {
+
+
+
   return <div className="Post">
-    <div className="Post-likes"></div>
-    <FiThumbsUp className="Thumb-icon" />
+    {/* <div className="Post-likes"></div> */}
+    <div className="Post-likes">{props.likes} </div>
+    {/* <input> */}
+    <FiThumbsUp key={props.id} className="Thumb-icon"
+      style={{ fill: props.liked ? "yellow" : "" }} onClick={() => {
+        props.toggleLiked(props.id)
+      }} />
+    {/* </input> */}
     <strong>{props.name}</strong>
     <br></br>
     <div className="Post-content">
       <strong>{props.program.length > 0 ? "Program" : ""}</strong>
       <br></br>
 
-      {props.program.map((workout) => (
-        <div className="Workout">
+      {props.program.map((workout, key) => (
+        <div className="Workout" key={key}>
           <br></br>
           <strong>{workout.workoutName}</strong>
           <br></br>
-          {workout.exercises.map((exercise) => (
-            <div className="Exercise">
+          {workout.exercises.map((exercise, key) => (
+            <div className="Exercise" key={key}>
               <strong>{exercise.name}</strong>
               <br></br>
               {exercise.sets} sets of {exercise.reps} reps
@@ -27,8 +41,12 @@ export function Post(props: { name: string, program: { workoutName: string; exer
         </div>
       ))}
 
-
       {props.image ? <><br></br> <img src={props.image} className="Post-image" alt="Exercise" /></> : ""}
+
+      <div className="Comment-icon">
+        Comment
+        <AiOutlineComment key={props.id} />
+      </div>
 
     </div>
   </div>;
