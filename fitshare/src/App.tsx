8f62618e96091a80b7c6b206e8d73f6f802ea356
Johanna Wilmers <johannawilmers@dhcp-10-24-7-115.wlan.ntnu.no>
2023-02-13 10:36:34 +0100

    feat: deployed the app to firebase, should be connected

--- fitshare/src/App.tsx
+++ fitshare/src/App.tsx
@@ -1,15 +1,14 @@
-import { Post } from './components/Post';
-import { Group } from './components/Group';
-import { Friend } from './components/Friend';
-import './App.css';
-import ExercisePhoto from './ExercisePhoto.jpeg';
-import FitShareLogo from './FitShareLogo.png';
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { Post } from "./components/Post";
+import { Group } from "./components/Group";
+import { Friend } from "./components/Friend";
+import "./App.css";
+import ExercisePhoto from "./ExercisePhoto.jpeg";
+import FitShareLogo from "./FitShareLogo.png";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-
-  const [currentUser, setCurrentUser] = useState("Meg")
+  const [currentUser, setCurrentUser] = useState("Meg");
 
   const [currentGroup, setCurrentGroup] = useState("Group 1");
 
@@ -40,89 +39,142 @@ function App() {
   ]);
 
   const [posts, setPosts] = useState([
-    { id: uuidv4(), name: "Gunnhild Pedersen", program: [{ workoutName: "Leg day", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }, { workoutName: "Workout 2", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }], image: ExercisePhoto, likes: 0, liked: false, comments: [{ person: "Roger", content: "Thats crazy!" }, { person: "Roger", content: "No way!" }, { person: "Kenneth", content: "That is the most crazy thing I have ever seen in my entire life! I really hope I can look just like you in the future! You are the person I dream of being in my sleep!" }, { person: "Sen", content: "I want you!" }] },
-    { id: uuidv4(), name: "Gunnhild Pedersen", program: [{ workoutName: "Pull", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }, { workoutName: "Workout 2", exercises: [{ name: "Bench Press", sets: 3, reps: 10 }, { name: "Squat", sets: 3, reps: 10 }] }], image: "", likes: 499, liked: true, comments: [] },
+    {
+      id: uuidv4(),
+      name: "Gunnhild Pedersen",
+      program: [
+        {
+          workoutName: "Leg day",
+          exercises: [
+            { name: "Bench Press", sets: 3, reps: 10 },
+            { name: "Squat", sets: 3, reps: 10 },
+          ],
+        },
+        {
+          workoutName: "Workout 2",
+          exercises: [
+            { name: "Bench Press", sets: 3, reps: 10 },
+            { name: "Squat", sets: 3, reps: 10 },
+          ],
+        },
+      ],
+      image: ExercisePhoto,
+      likes: 0,
+      liked: false,
+      comments: [
+        { person: "Roger", content: "Thats crazy!" },
+        { person: "Roger", content: "No way!" },
+        {
+          person: "Kenneth",
+          content:
+            "That is the most crazy thing I have ever seen in my entire life! I really hope I can look just like you in the future! You are the person I dream of being in my sleep!",
+        },
+        { person: "Sen", content: "I want you!" },
+      ],
+    },
+    {
+      id: uuidv4(),
+      name: "Gunnhild Pedersen",
+      program: [
+        {
+          workoutName: "Pull",
+          exercises: [
+            { name: "Bench Press", sets: 3, reps: 10 },
+            { name: "Squat", sets: 3, reps: 10 },
+          ],
+        },
+        {
+          workoutName: "Workout 2",
+          exercises: [
+            { name: "Bench Press", sets: 3, reps: 10 },
+            { name: "Squat", sets: 3, reps: 10 },
+          ],
+        },
+      ],
+      image: "",
+      likes: 499,
+      liked: true,
+      comments: [],
+    },
   ]);
 
   function toggleLiked(id: string) {
-    const newPosts = [...posts]
-    const post = newPosts.find(post => post.id === id)
+    const newPosts = [...posts];
+    const post = newPosts.find((post) => post.id === id);
 
     if (post != null) {
-      post.liked = !post.liked
+      post.liked = !post.liked;
       if (post.liked) {
-        post.likes += 1
+        post.likes += 1;
       } else {
-        post.likes -= 1
+        post.likes -= 1;
       }
     }
-    setPosts(newPosts)
+    setPosts(newPosts);
   }
 
   function addComment(id: string, comment: string) {
-    const newPosts = [...posts]
+    const newPosts = [...posts];
 
-    const post = newPosts.find(post => post.id === id)
+    const post = newPosts.find((post) => post.id === id);
 
     if (post && comment !== "") {
-      post.comments.push({ person: currentUser, content: comment })
+      post.comments.push({ person: currentUser, content: comment });
     }
 
-    setPosts(newPosts)
-    console.log(newPosts)
+    setPosts(newPosts);
+    console.log(newPosts);
   }
-
 
   return (
     <div className="App">
       {/* LEFT SIDE */}
       <div className="Left-side-bar">
-
-        <img className="FitSharelogo" src={FitShareLogo} alt="FitShareLogo">
-
-        </img>
+        <img
+          className="FitSharelogo"
+          src={FitShareLogo}
+          alt="FitShareLogo"
+        ></img>
         <div className="Groups">
           <strong>Groups</strong>
 
           {groups.map((group) => (
             <Group key={group.id} name={group.name} />
           ))}
-
         </div>
       </div>
 
       {/* MIDDLE */}
       <div className="Middle">
-        <div className="Top-bar">
-          {currentGroup}
-        </div>
+        <div className="Top-bar">{currentGroup}</div>
 
         <div className="Post-buttons">
-          <div className="Post-button">
-            Post Program
-          </div>
+          <div className="Post-button">Post Program</div>
 
-          <div className="Post-button">
-            Post Image
-          </div>
+          <div className="Post-button">Post Image</div>
         </div>
 
         <div className="Group-icon-feed">
-
           {posts.map((post) => (
-            <Post key={post.id} id={post.id} name={post.name} program={post.program} image={post.image}
-              likes={post.likes} liked={post.liked} comments={post.comments}
-              toggleLiked={toggleLiked} addComment={addComment} />
+            <Post
+              key={post.id}
+              id={post.id}
+              name={post.name}
+              program={post.program}
+              image={post.image}
+              likes={post.likes}
+              liked={post.liked}
+              comments={post.comments}
+              toggleLiked={toggleLiked}
+              addComment={addComment}
+            />
           ))}
-
         </div>
       </div>
 
       {/* RIGHT SIDE */}
       <div className="Right-side-bar">
-        <div className="Programs-button">
-          Programs
-        </div>
+        <div className="Programs-button">Programs</div>
 
         <div className="Friends">
           <strong>Friends</strong>
@@ -130,9 +182,7 @@ function App() {
           {friends.map((friend) => (
             <Friend key={friend.id} name={friend.name} />
           ))}
-
         </div>
-
       </div>
     </div>
   );
