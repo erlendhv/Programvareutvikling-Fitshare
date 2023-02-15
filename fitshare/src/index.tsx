301c9a5e2c02544d58a52c71a749d6f9e24418c9
Bendik Norli <bendik.norli@ntnu.no>
2023-02-15 13:08:59 +0100

    fix: removed bug disabling scrolling

--- fitshare/src/index.tsx
+++ fitshare/src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Main from './Main'
 
-document.body.style.overflow = "hidden";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
