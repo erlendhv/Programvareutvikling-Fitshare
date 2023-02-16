07b63199f404dab7d5382a260f26c7793c222b6c
Erlend Veire <erlendhv@stud.ntnu.no>
2023-02-16 16:26:29 +0100

    feat: connected login user to firebase

--- fitshare/src/Main.tsx
+++ fitshare/src/Main.tsx
@@ -4,11 +4,16 @@ import NewProgram from "./pages/NewProgram";
 import TrainingPrograms from "./pages/TrainingPrograms";
 import Error404page from './pages/Error404page';
 
-const Main = () => {
+import firebase from "firebase/compat/app"
+interface UserProps {
+  currentUser: firebase.User;
+}
+
+const Main: React.FC<UserProps> = ({ currentUser }) => {
   return (
     <BrowserRouter>
       <Routes> {/* The Switch decides which component to show based on the current URL.*/}
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<App currentUser={currentUser} />} />
         <Route path="/programs" element={<TrainingPrograms />} />
         <Route path="/newprogram" element={<NewProgram />} />
         <Route path="/*" element={<Error404page />} />
