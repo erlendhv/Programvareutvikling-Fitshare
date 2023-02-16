3d82015a2ddd93019b716d652b4687d40f63ff20
Kenneth Brattli <kennbr@stud.ntnu.no>
2023-02-16 11:42:48 +0100

    Revert "Merge branch '28-create-login-page' into 'main'"
    This reverts merge request !6

--- fitshare/src/index.tsx
+++ fitshare/src/index.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import LoginPage from "./pages/LoginPage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Main from './Main'
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <LoginPage></LoginPage>
+    <Main />
   </React.StrictMode>
 );
 
