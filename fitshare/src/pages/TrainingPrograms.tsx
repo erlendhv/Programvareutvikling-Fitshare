f5240bf1a15683072f2b04e1004cc59ebf3ba94e
Erlend Veire <erlendhv@stud.ntnu.no>
2023-02-21 22:36:50 +0100

    feat: programs are fetched from database

--- fitshare/src/pages/TrainingPrograms.tsx
+++ fitshare/src/pages/TrainingPrograms.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './../style/NewProgram.css';
 import { useNavigate } from 'react-router-dom';
 import { BiArrowBack } from 'react-icons/bi';
@@ -6,8 +6,9 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/analytics";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 
-interface Excersise {
+interface Exercise {
   name: string;
   sets: number;
   reps: number;
@@ -17,13 +18,13 @@ interface Excersise {
 interface Workout {
   id: string;
   name: string;
-  exercises: Excersise[];
+  exercises: String[];
 }
 
 interface Program {
   id: string;
   name: string;
-  workouts: Workout[];
+  workouts: string[];
 }
 
 export function TrainingPrograms(props: { currentUser: firebase.User }) {
@@ -43,93 +44,76 @@ export function TrainingPrograms(props: { currentUser: firebase.User }) {
   };
 
   const [programs, setPrograms] = useState<Program[]>([
-    // {
-    //   id: "0",
-    //   name: "Program 1",
-    //   workouts: [
-    //     {
-    //       id: "1",
-    //       name: "Workout 1",
-    //       exercises: [
-    //         {
-    //           name: "Bench Press",
-    //           sets: 3,
-    //           reps: 10,
-    //           id: "1",
-    //         },
-    //         {
-    //           name: "Squat",
-    //           sets: 3,
-    //           reps: 10,
-    //           id: "2",
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       id: "2",
-    //       name: "Workout 2",
-    //       exercises: [
-    //         {
-    //           name: "Bench Press",
-    //           sets: 3,
-    //           reps: 10,
-    //           id: "3",
-    //         },
-    //         {
-    //           name: "Squat",
-    //           sets: 3,
-    //           reps: 10,
-    //           id: "4",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
-    // {
-    //   id: "5",
-    //   name: "Program 2",
-    //   workouts: [
-    //     {
-    //       id: "8",
-    //       name: "Workout 1",
-    //       exercises: [
-    //         {
-    //           name: "Bench Press",
-    //           sets: 3,
-    //           reps: 10,
-    //           id: "6",
-    //         },
-    //         {
-    //           name: "Squat",
-    //           sets: 3,
-    //           reps: 10,
-    //           id: "7",
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       id: "10",
-    //       name: "Workout 2",
-    //       exercises: [
-    //         {
-    //           name: "Bench Press",
-    //           sets: 3,
-    //           reps: 10,
-    //           id: "9",
-    //         },
-    //         {
-    //           name: "Squat",
-    //           sets: 3,
-    //           reps: 10,
-    //           id: "10",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
+    {
+      id: "0",
+      name: "Your programs",
+      workouts: []
+    },
   ]);
 
-  const [currentProgram, setCurrentProgram] = useState<Program | Program>(programs[0]);
+  const [workouts, setWorkouts] = useState<Workout[]>([
+    {
+      id: "0",
+      name: "Your workouts",
+      exercises: []
+    },
+  ]);
+
+  const [exercises, setExercises] = useState<Exercise[]>([
+    {
+      id: "0",
+      name: "Exercise 1",
+      sets: 0,
+      reps: 0
+    },
+  ]);
+
+  const [currentProgram, setCurrentProgram] = useState<Program>(programs[0]);
+
+  const currentUserRef = firebase.firestore().collection("users").doc(props.currentUser.uid);
+  const [currentUserData] = useDocumentData(currentUserRef as any);
+
+  useEffect(() => {
+    let unsubscribe: firebase.Unsubscribe | undefined;
+    if (currentUserData) {
+      if (currentUserData.programs.length > 0) {
+        const programsRef = firebase.firestore().collection("programs").where(firebase.firestore.FieldPath.documentId(), "in", currentUserData.programs);
+        unsubscribe = programsRef.onSnapshot((querySnapshot) => {
+          const programs: any = [];
+          querySnapshot.forEach((doc) => {
+            programs.push(doc.data());
+          });
+          setPrograms(programs);
+        });
+      }
+      else {
+        setPrograms([]);
+      }
+      return () => {
+        if (unsubscribe) unsubscribe();
+      };
+    }
+  }, [currentUserData]);
+
+  // useEffect(() => {
+  //   console.log("useEffect");
+  //   console.log(currentProgram);
+  //   const matchingWorkouts: Workout[] = [];
+  //   currentProgram.workouts.forEach(async (workoutId) => {
+  //     const workoutCollection = firebase.firestore().collection("workout");
+  //     const querySnapshot = await workoutCollection.where('id', '==', workoutId).get();
+  //     querySnapshot.forEach((doc) => {
+  //       const workout = doc.data() as Workout;
+  //       matchingWorkouts.push(workout);
+  //       // workouts.push(workout);
+  //     });
+  //   }
+  //   );
+  //   // setWorkouts(workouts);
+  //   setWorkouts(matchingWorkouts);
+  //   console.log(workouts);
+  // }, [currentProgram]);
+
 
   return (
     <div className="NewProgram">
@@ -143,7 +127,7 @@ export function TrainingPrograms(props: { currentUser: firebase.User }) {
 
           {programs.map((program, key) => (
             <div key={key} className={currentProgram.id === program.id ? "Option-selected" : "Option"}
-              onClick={() => setCurrentProgram(program)}>
+              onClick={() => { setCurrentProgram(program) }}>
               {program.name}
             </div>
           ))}
@@ -151,11 +135,20 @@ export function TrainingPrograms(props: { currentUser: firebase.User }) {
         </div>
         <br></br>
         <div className="Overview">
-          <h2>Excecutions</h2>
+          <h2>Executions</h2>
           <div className="Create-new-button" onClick={addExecution}>Add new Execution</div>
+          {workouts.map((workoutBut, key) => (
+            <>
+              <div key={workoutBut.id} className="Option" >
+                {workoutBut.name}
+              </div>
+            </>
+          ))}
+
         </div>
+
       </div >
-    </div>
+    </div >
   );
 }
 
