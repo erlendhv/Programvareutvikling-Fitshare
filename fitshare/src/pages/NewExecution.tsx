724123602dd8966a39883ca1c75186986d2784df
Bendik Norli <bendik.norli@ntnu.no>
2023-03-15 13:57:56 +0100

    feat: visualize progress when logging exercises

--- fitshare/src/pages/NewExecution.tsx
+++ fitshare/src/pages/NewExecution.tsx
@@ -3,6 +3,7 @@ import { Group } from '../components/Group';
 import { Friend } from '../components/Friend';
 import { Popup } from '../components/Popup';
 import './../style/NewExecution.css';
+import './../style/NewProgram.css';
 import ExercisePhoto from '.m../ExercisePhoto.jpeg';
 import FitShareLogo from './../FitShareLogo.png';
 import { useState, useEffect, Key } from 'react';
@@ -16,6 +17,10 @@ import { BiArrowBack } from 'react-icons/bi';
 import { AiOutlineSearch } from 'react-icons/ai'
 import { GiWeightLiftingUp } from 'react-icons/gi'
 import { Execution } from '../components/Execution';
+
+import CanvasJS from '../components/canvasjs.react';
+import CanvasJSReact from '../components/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
 interface Exercise {
@@ -31,11 +36,11 @@ interface Execution {
     name: string;
     owner: string;
     weight: string;
-    date: Date;
+    date: firebase.firestore.Timestamp;
 }
 
 
-export function NewExecution(Props: { currentUser: firebase.User }) {
+export function NewExecution(props: { currentUser: firebase.User }) {
 
     const handleBack = () => {
         navigate('/programs');
@@ -49,7 +54,7 @@ export function NewExecution(Props: { currentUser: firebase.User }) {
             sets: 0,
             reps: 0,
             id: "1",
-            owner: Props.currentUser.uid,
+            owner: props.currentUser.uid,
         }
     ]);
 
@@ -61,6 +66,12 @@ export function NewExecution(Props: { currentUser: firebase.User }) {
 
     const [exerciseNames, setExerciseNames] = useState<string[]>([]);
 
+    const [selectedExercise, setSelectedExercise] = useState(exercises[0]);
+
+    const [previousExcecutions, setPreviousExcecutions] = useState<Execution[]>([]);
+
+    const [dataPoints, setDataPoints] = useState<{ x: number, y: number }[]>([]);
+
     const handleSearch = async () => {
         exerciseNames.splice(0);
         const usersCollection = firebase.firestore().collection("exercise");
@@ -68,7 +79,7 @@ export function NewExecution(Props: { currentUser: firebase.User }) {
         const matchingExercises: Exercise[] = [];
         querySnapshot.forEach((doc) => {
             const exercise = doc.data() as Exercise;
-            if (exercise.owner === Props.currentUser.uid && !exerciseNames.includes(exercise.name)) {
+            if (exercise.owner === props.currentUser.uid && !exerciseNames.includes(exercise.name)) {
                 matchingExercises.push(exercise);
                 exerciseNames.push(exercise.name);
             }
@@ -76,11 +87,10 @@ export function NewExecution(Props: { currentUser: firebase.User }) {
         setExercises(matchingExercises);
     };
 
-    const handleAddExecution = (exercise: Exercise) => {
-        console.log("Add execution");
+    const handleAddExecution = () => {
         if (weight !== "") {
-            setLoggedExercises([...loggedExercises, exercise]);
-            saveWeight(exercise);
+            setLoggedExercises([...loggedExercises, selectedExercise]);
+            saveWeight(selectedExercise);
         }
     };
 
@@ -88,9 +98,9 @@ export function NewExecution(Props: { currentUser: firebase.User }) {
         const newExecution: Execution = {
             id: uuidv4(),
             name: exercise.name,
-            owner: Props.currentUser.uid,
+            owner: props.currentUser.uid,
             weight: weight,
-            date: new Date(),
+            date: firebase.firestore.Timestamp.now(),
         };
 
         const executionCollection = firebase.firestore().collection("execution");
@@ -98,31 +108,109 @@ export function NewExecution(Props: { currentUser: firebase.User }) {
         setWeight("");
     }
 
+    useEffect(() => {
+        const executionCollection = firebase.firestore().collection("execution");
+        const query = executionCollection.where("owner", "==", props.currentUser.uid).where("name", "==", selectedExercise.name);
+
+        const querySnapshot = query.get();
+
+        querySnapshot.then((querySnapshot) => {
+            const matchingExecutions: Execution[] = [];
+            querySnapshot.forEach((doc) => {
+                const execution = doc.data() as Execution;
+                matchingExecutions.push(execution);
+            });
+
+            // Sort the executions by date
+            matchingExecutions.sort((a, b) => {
+                return a.date.toMillis() - b.date.toMillis();
+            });
+
+
+            // Create the datapoints for the graph
+            const dataPoints: { x: number, y: number }[] = [];
+
+            for (let i = 0; i < matchingExecutions.length; i++) {
+                dataPoints.push({ x: i, y: parseInt(matchingExecutions[i].weight) });
+            }
+
+            setDataPoints(dataPoints);
+        });
+
+
+    }, [selectedExercise, loggedExercises]);
+
     return (
         <div className="Execution-container">
             <BiArrowBack className="Back-button" onClick={handleBack} />
             <h1>New Execution</h1>
-            <div className="Searchbar-content">
-                <input className="Input-field" type="text" placeholder={"Exercise"} value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
-                <AiOutlineSearch className="Popup-search-icon" onClick={handleSearch} />
-            </div>
-            <div>
-                {exercises.map((exercise, key) => (
-                    <>
-                        {
-                            exercise.name.toLowerCase().includes(searchWord.toLowerCase()) ?
-                                <div key={key} className="Exercise-popup-inner">
-                                    <Execution name={exercise.name} />
-                                    <input className="Input-field" type="text" placeholder={"Weight"}
-                                        onChange={(e) => Number.isInteger(parseInt(e.target.value)) ? setWeight(e.target.value) : null} />
-                                    <div className="Log-execution-button"
-                                        onClick={() => handleAddExecution(exercise)}>{loggedExercises.includes(exercise) ? 'Logged' : 'Log'}</div>
-                                </div>
-                                : null
+            <div className="Overviews">
+                <div className="Overview">
+                    <div className="Searchbar-content">
+                        <input className="Input-field" type="text" placeholder={"Exercise"} value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
+                        <AiOutlineSearch className="Popup-search-icon" onClick={handleSearch} />
+                    </div>
+                    <div>
+                        {exercises.map((exercise, key) => (
+                            <>
+                                {
+                                    exercise.name.toLowerCase().includes(searchWord.toLowerCase()) ?
+                                        <div key={key} className="Exercise-popup-inner">
+                                            <Execution name={exercise.name}
+                                                onClick={() => { setSelectedExercise(exercise) }}
+                                                selected={selectedExercise === exercise}
+                                            />
+                                        </div>
+                                        : null
+                                }
+                            </>
+                        ))
                         }
-                    </>
-                ))
-                }
+
+
+                    </div>
+                </div>
+                <div className="Overview">
+                    <h2>Previous Executions</h2>
+                    <div className="Log-input">
+                        <input className="Input-field" placeholder="Weight (kg)" value={weight} onChange={(e) => {
+                            // Only allow numbers
+                            const re = /^[0-9\b]+$/;
+                            if (e.target.value === '' || re.test(e.target.value)) {
+                                setWeight(e.target.value)
+                            }
+                        }
+                        } />
+                        <div className="Log-execution-button"
+                            onClick={() => handleAddExecution()}>Log
+                        </div>
+                    </div>
+
+                    <CanvasJSChart options={{
+                        animationEnabled: true,
+                        exportEnabled: true,
+                        theme: "dark1", // "light1", "dark1", "dark2"
+                        title: {
+                            text: selectedExercise.name,
+                        },
+                        axisY: {
+                            title: "Weight",
+                            suffix: "kg"
+                        },
+                        axisX: {
+                            title: "Execution",
+                            interval: 1
+                        },
+                        data: [{
+                            type: "line",
+                            toolTipContent: "Execution {x}: {y}kg",
+                            dataPoints: dataPoints
+                        }]
+                    }}
+                    /* onRef={ref => this.chart = ref} */
+                    />
+
+                </div>
             </div>
         </div>
     );
