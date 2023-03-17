290dfca9938810d4686408c03960dc0c66ed9ae7
Vilde Brandal Avlesbug <vildeba@stud.ntnu.no>
2023-03-17 11:03:01 +0100

    Eksempelprogr. for weight loss og endurance
    Co-authored-by: kbrattli <kbrattli@users.noreply.github.com>

--- fitshare/src/components/RecommendedPost.tsx
+++ fitshare/src/components/RecommendedPost.tsx
@@ -31,13 +31,46 @@ export function RecommendedPost(props: UserProps) {
     return (<NonInteractablePost
       key={1}
       id={"1"}
-      name={"Lance Armstrong"}
-      description="WOOW"
+      name={"Recommended Weight Loss Program For You"}
+      description="Since you are interested in weight loss, we recommend this program. This program will help you increase metabolism over time, meaning you will burn more calories which can contribute to the calorie deficit required for weight loss."
       program={[{
-        workoutName: "NAME",
+        workoutName: "Lower Body Strength",
         exercises: [
-          { name: "Up the hill", sets: 3, reps: 10 },
-          { name: "Squat", sets: 3, reps: 10 },
+          { name: "Goblet squat", sets: 3, reps: 12 },
+          { name: "Deadlift", sets: 3, reps: 12 },
+          { name: "Walking lunge with dumbells (10 on each side)", sets: 3, reps: 20 },
+          { name: "Leg curl", sets: 3, reps: 12 },
+          { name: "Leg extension", sets: 3, reps: 12 },
+        ]
+      }, {
+        workoutName: "Low intensity cardio",
+        exercises: [
+          { name: "Walking, light jogging, or your preferred method of cardio for at least 20 minutes", sets: 1, reps: 1 },
+        ]
+      }, {
+        workoutName: "Upper Body Strenght",
+        exercises: [
+          { name: "Lat pulldown", sets: 3, reps: 12 },
+          { name: "Dumbbell overhead press", sets: 3, reps: 12 },
+          { name: "Dumbbell row", sets: 3, reps: 12 },
+          { name: "Dumbbell bench press", sets: 3, reps: 12 },
+          { name: "Biceps curl", sets: 3, reps: 12 },
+          { name: "Triceps extension", sets: 3, reps: 12 },
+        ]
+      }, {
+        workoutName: "Low intensity cardio",
+        exercises: [
+          { name: "Walking, light jogging, or your preferred method of cardio for at least 20 minutes", sets: 1, reps: 1 },
+        ]
+      }, {
+        workoutName: "Interval conditioning (perform each exercise at a hard pace)",
+        exercises: [
+          { name: "Jump squat", sets: 3, reps: 15 },
+          { name: "Dumbbell overhead press", sets: 3, reps: 15 },
+          { name: "Dumbbell row", sets: 3, reps: 15 },
+          { name: "Dumbbell bench press", sets: 3, reps: 15 },
+          { name: "Biceps curl", sets: 3, reps: 15 },
+          { name: "Triceps extension", sets: 3, reps: 15 },
         ]
       }]}
       image={"https://www.cyclingweekly.com/wp-content/uploads/2020/09/lance-armstrong-1.jpg"}
@@ -96,20 +129,37 @@ export function RecommendedPost(props: UserProps) {
     return (<NonInteractablePost
       key={3}
       id={"3"}
-      name={"Roger Federer"}
-      description=""
+      name={"Recommended Endurance Program For You"}
+      description="Since you are interested in endurance training, we recommended this program for you. It is a 3 day split program that focuses on improving your endurance."
       program={[{
-        workoutName: "Tennis",
+        workoutName: "Interval training",
         exercises: [
-          { name: "Up the hill", sets: 3, reps: 10 },
-          { name: "Squat", sets: 3, reps: 10 },
+          { name: "Warm up: 5-10 minutes of light cardio (e.g. jogging, cycling)", sets: 1, reps: 1 },
+          { name: "Intervals: Alternate between 30 seconds of all-out effort (e.g. sprints, jumping jacks) and 30 seconds of active recovery (e.g. jogging, walking) for a total of 20-30 minutes", sets: 1, reps: 1 },
+          { name: "Cool-down: 5-10 minutes of stretching", sets: 1, reps: 1 }
         ]
-      }]}
+      }, {
+        workoutName: "Steady-State Cardio (30 seconds rest between each exercise",
+        exercises: [
+          { name: "Warm up: 5-10 minutes of light cardio (e.g. jogging, cycling)", sets: 1, reps: 1 },
+          { name: "Steady-State Cardio: 30-60 minutes of continuous cardio at a moderate intensity (e.g. jogging, cycling, swimming)", sets: 1, reps: 1 },
+          { name: "Cool-down: 5-10 minutes of stretching", sets: 1, reps: 1 }
+        ]
+      }, {
+        workoutName: "Hill repeats",
+        exercises: [
+          { name: "Warm up: 5-10 minutes of light cardio", sets: 1, reps: 1 },
+          { name: "Run as fast as you can up the hill, walk or jog down. Rest for 3 minutes betweeen each set", sets: 3, reps: 10 },
+          { name: "Cool-down: 5-10 minutes of stretching", sets: 1, reps: 1 }
+        ]
+      }
+      ]}
       image={"https://firebasestorage.googleapis.com/v0/b/fitshare-7b3ca.appspot.com/o/images%2FRipped%20Guy.webp?alt=media&token=41327429-4dbc-4303-b70c-10ee8c8b57c8"}
     />)
   }
   return (<div></div>)
 }
+
 
 function NonInteractablePost(props: {
   id: string,
